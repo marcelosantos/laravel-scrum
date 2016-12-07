2ef83d88a48a50b68f0eac02933b76139b2376ca
Marcelo Santos <marcelo.santos@fortaleza.ce.gov.br>
2016-12-07 16:23:59 -0300

    novos models, seeds e migrations

---
+++ database/migrations/2016_12_07_162925_create_cards_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCardsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('list_id')->unsigned();
+            $table->foreign('list_id')->references('id')->on('listings');
+            $table->string('title');
+            $table->text('description');
+            $table->integer('story_points');
+            $table->integer('business_value');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('lists', function(){
+    return App\Listing::all();
+});
